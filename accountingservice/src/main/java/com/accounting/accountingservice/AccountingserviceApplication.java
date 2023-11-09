b6d864411e01d227eaf8c74879a0f1496dce8db1
AG7882 <88968653+AG7882@users.noreply.github.com>
2023-11-09 06:45:02 +0500

    Add files via upload

---
+++ accountingservice/src/main/java/com/accounting/accountingservice/AccountingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.accountingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingserviceApplication.class, args);
+	}
+
+}
